a209c0ac698cf68fb14694dff69ae4d3bcdf5671
ZonianMidian <57513632+ZonianMidian@users.noreply.github.com>
2025-07-31 03:55:52 -0500

    fix: added missing logo on home preview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<svelte:head>
+	<meta property="og:image" content="{page.url.origin}/favicon.png" />
+</svelte:head>
+
 <div class="box-border flex max-h-[90vh] justify-center p-8">
 	<img src="/logo.svg" alt="Logo" class="max-h-full max-w-full object-contain" />
 </div>
